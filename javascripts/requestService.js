57b53ce75971abc9573bf82a5a748f5a9ce65830
Tommy Waters <Thomas.Waters05@gmail.com>
2017-01-11 19:16:45 -0600

    adding to complete this, no idea why this isnt all pushing at once

---
+++ javascripts/requestService.js
@@ -0,0 +1,9 @@
+;(function () {
+  angular.module('fetchApp').service('requestService', function ($http) {
+    this.get = (url) => $http.get(url);
+    this.post = (url) => $http.post(url,data);
+    this.head = (url) => $http.head(url);
+    this.options = (url) => $http.options(url);
+
+  });
+})();
