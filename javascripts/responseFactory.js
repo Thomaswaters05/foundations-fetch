2e2acbef201082ca80d6bb4c62214091d93d5805
Tommy Waters <Thomas.Waters05@gmail.com>
2017-01-14 01:25:38 -0600

    getting URL + method to print to DOM, also created a table for styling- really confused on the main controller and what to do?

--- javascripts/responseFactory.js
+++ javascripts/responseFactory.js
@@ -2,7 +2,11 @@
   angular.module('fetchApp').factory('Response', function () {
     const Response = function (responseData) {
       this.statusCode = responseData.status;
-      console.log(responseData);
+      console.log("complete object",responseData);
+      this.responseURL = responseData.config.url;
+      console.log("responseURL",responseData.config.url)
+      this.method = responseData.config.method;
+      console.log("method", responseData.config.method);
     };
 
     return Response;
