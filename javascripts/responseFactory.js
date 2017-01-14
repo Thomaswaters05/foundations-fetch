094ccfaad045fce9a910a7f1d903658032feb0be
Tommy Waters <Thomas.Waters05@gmail.com>
2017-01-14 01:45:24 -0600

    added what I believe is the length, attempting the time before moving on the time calculation

--- javascripts/responseFactory.js
+++ javascripts/responseFactory.js
@@ -7,6 +7,9 @@
       console.log("responseURL",responseData.config.url)
       this.method = responseData.config.method;
       console.log("method", responseData.config.method);
+      this.contentLength = responseData.headers.length;
+      console.log("length", responseData.headers.length);
+      this.contentTime =
     };
 
     return Response;
